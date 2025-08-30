46bb3f8a09f6bd9e111eda1fa52ccca05758881a
Doomturtles <jonny.collins111@gmail.com>
2025-08-30 18:35:29 +0100

    locked scene view and adjusted car movement

--- src/Vehicles/Car.ts
+++ src/Vehicles/Car.ts
@@ -1,20 +1,17 @@
 import { Mesh, MeshBuilder, Scene, Vector3 } from '@babylonjs/core';
 import {Vehicle} from './Vehicle'
 export class Car extends Vehicle{
-    private lifetime: number;
     Move(): void {
-        this.lifetime += 1;
         this.mesh.position.z += 0.1;
     }
     constructor(scene: Scene){
         const myMesh: Mesh = MeshBuilder.CreateCapsule("CarMesh", {height: 5, radiusBottom: 2, radiusTop: 2, radius: 3, subdivisions: 10}, scene);
-        myMesh.rotation.x = 90;
         super(myMesh)
-        this.lifetime = 0;
+        this.mesh.position.z = -20
     }
 
     IsDone(): boolean{
-        return this.lifetime > 200;
+        return this.mesh.position.z > 20;
     }
 
     DisposeOfMesh(): void{
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, MeshBuilder, Vector3 } from "@babylonjs/core";
+import { Engine, Scene, UniversalCamera, HemisphericLight, MeshBuilder, Vector3 } from "@babylonjs/core";
 import {VehicleManager} from "./Vehicles/VehicleManager"
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 const engine = new Engine(canvas, true);
 
 const scene = new Scene(engine);
 
-const camera = new ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 5, Vector3.Zero(), scene);
-camera.attachControl(canvas, true);
-
+const camera = new UniversalCamera("main", new Vector3(15, 15, 15), scene);
+camera.setTarget(new Vector3(0,0,0))
 new HemisphericLight("light", new Vector3(0.5, 1, 0), scene);
 
 const vehManager: VehicleManager = new VehicleManager();
